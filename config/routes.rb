e95edfcdca04c9500996fccde752d8b6ed788783
Samreen Malla <samreen.malla@gmail.com>
2017-12-13 23:55:47 +0545

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'ideas#index'
+
+  resources :ideas
+
+ 	get 'about' => 'static_pages#about'
+	get 'random' => 'static_pages#random'
+end
